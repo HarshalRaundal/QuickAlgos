3cfd7bed14121ad55902260915061d6327808ac5
Harshal <yournewemail@example.com>
2025-07-06 15:34:42 +0530

    Added bitwise operator to check set bits in exponent.

--- BinaryExponentiation/Base.java
+++ BinaryExponentiation/Base.java
@@ -29,6 +29,18 @@ public class Base {
         return result;
     }
 
+    private long powerIterativeBitwise(long base, long exponent) {
+        long result = 1;
+        while (exponent > 0) {
+            if ((exponent & 1) == 1) { // Check if the least significant bit is set
+                result *= base;
+            }
+            base *= base;
+            exponent >>= 1; // Right shift exponent by 1 (equivalent to dividing by 2)
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter base and exponent:");
@@ -37,5 +49,6 @@ public class Base {
         Base b = new Base();
         System.out.printf("Power of %d raised to %d is %d%n", base, exponent, b.powerRecurive(base, exponent));
         System.out.printf("Power of %d raised to %d is %d%n", base, exponent, b.powerIterative(base, exponent));
+        System.out.printf("Power of %d raised to %d is %d%n", base, exponent, b.powerIterativeBitwise(base, exponent));
     }
 }
